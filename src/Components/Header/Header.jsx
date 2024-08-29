737c69295f0144dab6e8bb71e6af8c0a3741035f
Sumit Jangir <jangirsumit1011@gmail.com>
2024-08-29 09:34:35 +0530

    Apply Router

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,32 +1,74 @@
-import React from 'react'
-import logo from '../../assets/img/logo.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/img/logo.png";
 
 const Header = () => {
   return (
     <>
-     <section id="header">
-        <a href="#"><img src={logo} alt="Logo" /></a>
+      <section id="header">
+        <Link to={"/"}>
+          <img src={logo} alt="Logo" />
+        </Link>
 
         <div>
-            <ul id="navbar">
-                <li><a className="active" href="index.html">Home</a></li>
-                <li><a href="shop.html">Shop</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li className="lg-bag"><a href="cart.html"><i className="fa-solid fa-bag-shopping"></i></a></li>
-                <a href="#" id="close"><i className="fa-solid fa-xmark"></i></a>
-            </ul>
+          <ul id="navbar">
+            <li>
+              {/* <a className="active" href="index.html">Home</a> */}
+              <Link className="active nav-link" to={"/"}>
+                HOME
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to={"/shop/"}>
+                Shop
+              </Link>
+              {/* <a href="shop.html">Shop</a> */}
+            </li>
+            <li>
+              <Link className="nav-link" to={"/Blog/"}>
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to={"/about/"}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to={"/contact/"}>
+                Contact
+              </Link>
+            </li>
+            <li className="lg-bag">
+              <Link className="nav-link" to={"/cart/"}>
+                <i className="fa-solid fa-bag-shopping"></i>
+              </Link>
+            </li>
+            <a href="#" id="close">
+              <i className="fa-solid fa-xmark"></i>
+            </a>
+            <li>
+              <Link className="login-btn" to={"/login/"}>
+                Login
+              </Link>
+            </li>
+            <li style={{padding:"0px"}}>
+              <Link className="login-btn" to={"/signup/"}>
+                SignUp
+              </Link>
+            </li>
+          </ul>
         </div>
-        
+
         <div id="mobile">
-            <a href="cart.html"><i className="fa fa-shopping-bag" aria-hidden="true"></i></a>
-            <i id="bar" className="fas fa-outdent"></i>
-            </div>
-
-    </section>
+          <a href="cart.html">
+            <i className="fa fa-shopping-bag" aria-hidden="true"></i>
+          </a>
+          <i id="bar" className="fas fa-outdent"></i>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
