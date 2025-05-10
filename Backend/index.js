cd5c580d6983bcc2db8460326600c1bd5fd30972
Sumit Jangir <jangirsumit1011@gmail.com>
2025-05-10 23:46:33 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -32,7 +32,9 @@ mongoose.connect(process.env.MONGOURL)
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
+
+  export const handler = serverless(app);
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
